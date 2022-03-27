b48b0a907d01bc71fd98439d53bef768c8d95022
mazunki <rolferen@gmail.com>
2022-03-27 20:25:11 +0200

    displays full list

--- atomic.py
+++ atomic.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 class Subshell:
+    all_subshells = ["1s", "2s", "2p", "3s", "3p", "4s", "3d", "4p", "5s", "4d", "5p", "6s", "4f", "5d", "6p", "7s", "5f", "6d", "7p"]
     capacities = { "s": 2, "p": 6, "d": 10, "f": 14 }
     def __init__(self, n, subshell):
         self.n = n
@@ -13,27 +14,36 @@ class Subshell:
         for exp in range(1, self.capacity+1):
             yield f"{self.n}{self.subshell}{exp}"
 
-class Atom:
-    def __init__(self, atomic_number, name=None):
-        self.atomic_number = atomic_number
-        self.name = name
-        self.electron_configuration = self.stable_electrons
-
     @staticmethod
-    def generate_configurations():
-        for n, subshell in ["1s", "2s", "2p", "3s", "3p", "4s", "3d", "4p", "5s", "4d", "5p", "6s", "4f", "5d", "6p", "7s", "5f", "6d", "7p"]:
+    def get_all_locations():
+        for n, subshell in Subshell.all_subshells:
             for position in Subshell(int(n), subshell):
                 yield position
 
-    @property
-    def stable_electrons(self):
-        return "123"
+class Atom:
+    def __init__(self, atomic_number, name=None):
+        self.atomic_number = self.free_electrons = atomic_number
+        self.name = name
+        self.electron_configuration = []
+        self.restabilize_electrons()
 
+    def restabilize_electrons(self):
+        free_electrons = self.atomic_number
+        self.electron_configuration = []
+
+        for location in Subshell.get_all_locations():
+            self.add_electron(location)
+            if not self.free_electrons:
+                return
+
+    def add_electron(self, location):
+        self.electron_configuration.append(location)
+        self.free_electrons -= 1
 
 if __name__ == "__main__":
-    configurations = Atom.generate_configurations()
-    print([ str(c) for c in configurations ])
+    configurations = Subshell.get_all_locations()
+    print(list(configurations))
     
     atomic_number = int(input("Enter atomic number: "))
     atom = Atom(atomic_number)
-    print(atom.stable_electrons)
+    print(atom.electron_configuration)
